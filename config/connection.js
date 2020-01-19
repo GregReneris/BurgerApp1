28d64e5892b0777e3543b877fc1c3cebb453707c
Gregory Reneris <greg@reneris.com>
2020-01-19 15:43:44 -0800

    This is when we add the JAWSDB database stuff. If everything explodes, roll back to the one before this.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// old connection code:
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
+var connection;
 
+if(process.env.JAWSDB_URL){
+  connection - mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -18,5 +31,13 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+//Maybe the below?
+// app.get('/',(req,res)=>{
+//   // will serve tables page
+//   res.sendFile(path.join(__dirname, "../views/layouts/main.handlebars"));
+// })
+
 // Export connection for our ORM to use.
+
+
 module.exports = connection;
